d9a0a58aefc5628c8b2ee35ec9876e64b83740d6
HaeWoal <newnest02@naver.com>
2022-08-30 22:24:43 +0900

    23
    23

--- Engine/Private/GameInstance.cpp
+++ Engine/Private/GameInstance.cpp
@@ -12,7 +12,7 @@ CGameInstance::CGameInstance()
 	//, m_pKeyComponent_Manager(CComponent_Manager::Get_Instance())
 
 {
-	//sdsd
+	//sdsdsdsds
 	//TEXT
 	//Safe_AddRef(m_pKeyComponent_Manager);
 	Safe_AddRef(m_pComponent_Manager);
